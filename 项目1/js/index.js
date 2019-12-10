a0c4509a1de551ac218d44bfc71123b76a7ecc51
572397420 <572397420@qq.com>
2019-12-10 20:24:19 +0800

    这是放大镜和商品详情

---
+++ 项目1/js/index.js
@@ -0,0 +1,20 @@
+$(".cc").children(".ca").hover(function(){
+    $(this).children("dl").css({
+        width:100,
+        position:"absolute",
+        zIndex:99999,
+        display:"block",
+        left:200
+    })
+    .eq($(this).index()).css({
+        display:"none",
+    });
+    $(this).children("dd").css(
+        {
+            display:"inline"
+        }
+    );
+},function(){
+    $(this).children("dl").css("display","none").eq($(this).index()).css("display","none");
+    $(this).children("dd").css("display","none"); 
+})
